6322de02ccc93445fbdac87d554c9fc4fadf1a1b
Alex Gap <alex.gap@gmail.com>
2022-06-20 14:15:19 -0700

    ignore stream reset error Using a slightly ugly hack for now

--- message-service.go
+++ message-service.go
@@ -107,7 +107,8 @@ func NewP2PMessageService(me MyInfo, peers map[types.Address]PeerInfo, metrics *
 				// Create a buffer stream for non blocking read and write.
 				raw, err := reader.ReadString(DELIMETER)
 				// TODO: If the stream has been closed we just bail for now
-				if errors.Is(err, io.EOF) || (errors.Is(err, errors.New("stream reset"))) {
+				// TODO: Properly check for and handle stream reset error
+				if errors.Is(err, io.EOF) || fmt.Sprintf("%s", err) == "stream reset" {
 					stream.Close()
 					return
 				}
