0e0a01fd667a367c28fc2bb5563a0b801e3d28ee
Alex Gap <alex.gap@gmail.com>
2022-09-21 14:52:39 -0700

    include additional version info

--- utils/testing.go
+++ utils/testing.go
@@ -198,16 +198,24 @@ func GenerateVirtualFundObjectiveRequest(me, payee, hub types.Address) virtualfu
 // RecordRunInfo records a single point using the metrics API tagged with the various parameters and versions for the run.
 func RecordRunInfo(me peer.MyInfo, config config.RunConfig, metrics *runtime.MetricsApi) {
 	info, _ := debug.ReadBuildInfo()
-
-	var version string
+	testVersion := info.Main.Version
+	var nitroVersion string
+	var tgVersion string
+	var tgSdkVersion string
 	for _, dep := range info.Deps {
-		if dep.Path == "github.com/statechannels/go-nitro" {
-			version = dep.Version
+		switch dep.Path {
+		case "github.com/statechannels/go-nitro":
+			nitroVersion = dep.Version
+		case "github.com/testground/testground":
+			tgVersion = dep.Version
+		case "github.com/testground/sdk-go":
+			tgSdkVersion = dep.Version
 		}
 	}
 
-	runDetails := fmt.Sprintf("nitroVersion=%s,me=%s,role=%v,hubs=%d,payers=%d,payees=%d,payeePayers=%d,duration=%s,concurrentJobs=%d,jitter=%d,latency=%d",
-		version, me.Address, me.Role, config.NumHubs, config.NumPayers,
+	runDetails := fmt.Sprintf("nitroVersion=%s,testVersion=%s,tgVersion=%s,tgSdkVersion=%s,me=%s,role=%v,hubs=%d,payers=%d,payees=%d,payeePayers=%d,duration=%s,concurrentJobs=%d,jitter=%d,latency=%d",
+		nitroVersion, testVersion, tgVersion, tgSdkVersion,
+		me.Address, me.Role, config.NumHubs, config.NumPayers,
 		config.NumPayees, config.NumPayeePayers,
 		config.PaymentTestDuration, config.ConcurrentPaymentJobs,
 		config.NetworkJitter.Milliseconds(), config.NetworkLatency.Milliseconds())
