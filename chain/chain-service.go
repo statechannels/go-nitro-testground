9b03f586905eb7e8780359cfb297f11d7b7f63a5
Alex Gap <alex.gap@gmail.com>
2022-10-19 14:28:33 -0700

    switch to fevm chain service

--- chain/chain-service.go
+++ chain/chain-service.go
@@ -2,77 +2,25 @@ package chain
 
 import (
 	"context"
-	"encoding/hex"
 	"io"
 	"log"
-	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/statechannels/go-nitro/client/engine/chainservice"
-	NitroAdjudicator "github.com/statechannels/go-nitro/client/engine/chainservice/adjudicator"
-	Create2Deployer "github.com/statechannels/go-nitro/client/engine/chainservice/create2deployer"
 )
 
 func NewChainService(ctx context.Context, seq int64, logDestination io.Writer) chainservice.ChainService {
-	client, err := ethclient.Dial("ws://hardhat:8545/")
+	fcs, err := chainservice.NewFevmChainService("https://wallaby.node.glif.io/rpc/v0", "9182b5bf5b9c966e001934ebaf008f65516290cef6e3069d11e718cbd4336aae", log.Default().Writer())
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-
-	txSubmitter, err := bind.NewKeyedTransactorWithChainID(getFundedPrivateKey(uint(seq)), big.NewInt(1337))
-	if err != nil {
-		log.Fatal(err)
-	}
-	txSubmitter.GasLimit = uint64(30_000_000) // in units
-
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	txSubmitter.GasPrice = gasPrice
-
-	deployer, err := Create2Deployer.NewCreate2Deployer(common.HexToAddress("0x5fbdb2315678afecb367f032d93f642f64180aa3"), client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hexBytecode, err := hex.DecodeString(NitroAdjudicator.NitroAdjudicatorMetaData.Bin[2:])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	naAddress, err := deployer.ComputeAddress(&bind.CallOpts{}, [32]byte{}, crypto.Keccak256Hash(hexBytecode))
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// One testground instance attempts to deploy NitroAdjudicator
 	if seq == 1 {
-		bytecode, err := client.CodeAt(ctx, naAddress, nil) // nil is latest block
+		err = fcs.DeployAdjudicator()
 		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Has NitroAdjudicator been deployed? If not, deploy it.
-		if len(bytecode) == 0 {
-			_, err = deployer.Deploy(txSubmitter, big.NewInt(0), [32]byte{}, hexBytecode)
-			if err != nil {
-				log.Fatal(err)
-			}
+			panic(err)
 		}
 	}
 
-	na, err := NitroAdjudicator.NewNitroAdjudicator(naAddress, client)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return fcs
 
-	cs, err := chainservice.NewEthChainService(client, na, naAddress, common.Address{}, common.Address{}, txSubmitter, logDestination)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return cs
 }
